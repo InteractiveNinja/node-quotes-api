17cc899a11ff72d4a71187fe04e64db28ea5168f
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-02-15 15:19:32 +0100

    added headers

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express from 'express'
 
 import {config} from '@interactiveninja/config-reader'
-
+import cors from 'cors'
 import { getQ } from './getQuote';
 
 let manager = config("config.json")
 let app = express()
 
+app.use(cors())
+
 const PORT = manager.get("port")
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get("/",(res,req) =>{
     getQ().then(val => req.json(val)).catch(e => req.json(e))
